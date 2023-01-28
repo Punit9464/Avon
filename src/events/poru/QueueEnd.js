cbcfbcd379c26d8cd11ddba4bdd0aaa72106021b
Punit Kumar <punitkumar8728@gmail.com>
2023-01-28 06:51:04 +0000

    commit

--- src/events/poru/QueueEnd.js
+++ src/events/poru/QueueEnd.js
@@ -13,20 +13,15 @@ class QueueEnd extends AvonClientEvent{
         if(!db || db === null) this.client.data.set(`${player.guildId}-autoPlay`,`disbaled`);
         if(db === `enabled`)
         {
-            let previous = player.previousTrack;
-            if(!previous) return;
-            let id = previous.info.identifier;
-            const search = `https://www.youtube.com/watch?v=${id}&list=RD${idr}`;
-            let response = await this.client.poru.resolve(search);
-            const { tracks } = response;
-            player.queue.add(tracks[Math.floor(Math.random() * Math.floor(response.tracks.length))]);
-            player.play();
+            player.autoplay(true);
         }
+        if(db === `disabled`){
         let guild = this.client.guilds.cache.get(player.guildId);
         let em = new EmbedBuilder().setColor(this.client.config.color).setAuthor({name : `| Queue Concluded` , iconURL : guild.iconURL({dynamic : true})});
         let but1 = new ButtonBuilder().setStyle(ButtonStyle.Link).setURL(`https://top.gg/bot/904317141866647592/vote`);
         let row = new ActionRowBuilder().addComponents(but1);
         await ch.send({embeds : [em],components : [row]});
+        }
         } catch(e) { }
     }
 }
